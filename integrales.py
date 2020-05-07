13d8581bd0cac670f3709f42742c6e35774dd200
trini-gonzalezpadin <trinigonzalezpadin@gmail.com>
2020-05-07 10:27:42 -0300

    trini

---
+++ integrales.py
@@ -0,0 +1,15 @@
+
+#practica de integrales
+import sympy as sp 
+from sympy import Symbol
+from sympy import integrate
+x = sp.Symbol('x')
+y=3*x
+print(sp.integrate(y,(x,5,7)))
+
+
+import numpy as np
+import sympy as sp 
+from sympy import Symbol
+from sympy import integrate
+import math 

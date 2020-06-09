bffc4c6121cc517fd8d05edf786b26b941ddbe04
trini-gonzalezpadin <trinigonzalezpadin@gmail.com>
2020-06-08 22:48:55 -0300

    trini

--- proyecto.py
+++ proyecto.py
@@ -22,12 +22,15 @@ print('s2: Base trigonométrica:\n\tf1(x)=cos(pix)\n\t' +
           'f2(x)=sen(pix)\n\tf3(x)=1')
 print('s3: Polinomios de grado impar hasta 5 :\n\tf1(x)=x' +
           '\n\tf2(x)=x^3\n\tf3(x)=x^5')
+print('s4: Recta:\n\tf1(x)=1\n\t' +
+          'f2(x)=x')
 
 print('Elija su opcion ')
 op = int(input())
 
 
 #pasar Funciones del subespacio a expresion
+
 #s1
 funcion11='1'
 funcion11=sympify(funcion11)
@@ -50,122 +53,183 @@ funcion32=sympify(funcion32)
 funcion33='x**5'
 funcion33=sympify(funcion31)
 
+#s4
+funcion41='1'
+funcion41=sympify(funcion41)
+funcion42='x'
+funcion42=sympify(funcion42)
+
+
 #para la opcion 1: subespacio polinomios grado menor o igual a 2 
 #coeficientes de la matriz asociada para sub1 
 #cada coefinciente es el producto escalar usual que es la integral de un prodcuto de funciones
 if op==1:
+    # Genero las matrices, una columna y otra cuadrada de 3x3, vacias:
+    matrizB = np.zeros(3)
+    matrizA = np.zeros((3, 3))
     y11=funcion11*funcion11
-    a11=sp.integrate(y11,(x,c,d))
-
     y12=funcion11*funcion12
-    a12=sp.integrate(y12,(x,c,d))
-
     y13=funcion11*funcion13
-    a13=sp.integrate(y13,(x,c,d))
-
     y22=funcion12*funcion12
-    a22=sp.integrate(y22,(x,c,d))
-
     y23=funcion12*funcion13
-    a23=sp.integrate(y23,(x,c,d))
-
     y33=funcion13*funcion13
-    a33=sp.integrate(y33,(x,c,d))
+    
+    # Calculo de Matriz A:
+    # para definir las funciones que van en la intregal
+    # las calcule cada una.
 
-    #matriz de coeficientes del sistema 
-    mat_A=np.array([[a11,a12,a13],[a12,a22,a23],[a13,a23,a33]])
-    print(mat_A)
+    matrizA[0][0] = sp.integrate(y11,(x,c,d))
+    matrizA[0][1] = sp.integrate(y12,(x,c,d))
+    matrizA[0][2] = sp.integrate(y13,(x,c,d))
 
-    #matriz columna de terminos independientes 
+    matrizA[1][0] = sp.integrate(y12,(x,c,d))
+    matrizA[1][1] = sp.integrate(y22,(x,c,d))
+    matrizA[1][2] = sp.integrate(y23,(x,c,d))
 
+    matrizA[2][0] = sp.integrate(y13,(x,c,d))
+    matrizA[2][1] =sp.integrate(y23,(x,c,d))
+    matrizA[2][2] = sp.integrate(y33,(x,c,d))
+            
+    print('Matriz A = ',matrizA)
+    #matriz columna B
     l1=funcion*funcion11
-    b1=sp.integrate(l1,(x,c,d))
     l2=funcion*funcion12
-    b2=sp.integrate(l2,(x,c,d))
     l3=funcion*funcion13
-    b3=sp.integrate(l3,(x,c,d))
-    mat_B=np.array([b1,b2,b3])
-    print(mat_B)
-    # resolver el sistema 
-    mat_A_inv = np.linalg.inv(mat_A)
-    print('La matriz inversa asociada al sistema es:')
-    print(mat_A_inv)
-    res= np.matmul(mat_A_inv, mat_B)
-    print(res)
-    print('La solución del sistema asociado al problemas es: {res}')
+    
+    matrizB[0] = sp.integrate(l1,(x,c,d))
+    matrizB[1] = sp.integrate(l2,(x,c,d))
+    matrizB[2] = sp.integrate(l3,(x,c,d))
 
+    print('Matriz B = ',matrizB)
+    
+    # Resuelvo el sistema de ecuaciones
+    matrizAInv = np.linalg.inv(matrizA)
+    matrizX = np.matmul(matrizAInv,matrizB)
+
+    print('Coeficientes = ',matrizX)
+    print ('La mejor aproximacion de la funicon es:')
 else:
     if op==2:
+        # Genero las matrices, una columna y otra cuadrada , vacias:
+        matrizB = np.zeros(2)
+        matrizA = np.zeros((2, 2))
+
 
         y11=funcion21*funcion21
-        a11=sp.integrate(y11,(x,c,d))
-
         y12=funcion21*funcion22
-        a12=sp.integrate(y12,(x,c,d))
-
-
         y22=funcion22*funcion22
-        a22=sp.integrate(y22,(x,c,d))
 
-        m1 = np.array([[a11,a12],[a12,a22]])
+        matrizA[0][0] = sp.integrate(y11,(x,c,d))
+        matrizA[0][1] = sp.integrate(y12,(x,c,d))
+        matrizA[1][0] = sp.integrate(y12,(x,c,d))
+        matrizA[1][1] = sp.integrate(y22,(x,c,d))
+        print('Matriz A = ',matrizA)
 
-        #matriz columna de terminos independientes 
+            
+        #matriz columna B 
+        l1=funcion*funcion41
+        l2=funcion*funcion42
 
-        l1=funcion*funcion21
-        b1=sp.integrate(l1,(x,c,d))
+        matrizB[0] = sp.integrate(l1,(x,c,d))
+        matrizB[1] = sp.integrate(l2,(x,c,d))
+        print('Matriz B = ',matrizB)
 
+        # Resuelvo el sistema de ecuaciones
+        matrizAInv = np.linalg.inv(matrizA)
+        matrizX = np.matmul(matrizAInv,matrizB)
 
-        l2=funcion*funcion22
-        b2=sp.integrate(l2,(x,c,d))
+        print('Coeficientes = ',matrizX)
+        print ('La mejor aproximacion de la funicon es:')
 
-        m2 = np.array([b1,b2])
-
-        # resolver el sistema 
-        z=np.linalg.solve(m1,m2)
-        print(z)
-        print('La solución del sistema asociado al problemas es: {z}')
-
-
-
+  
     else:
-        y11=funcion31*funcion31
-        a11=sp.integrate(y11,(x,c,d))
+        if op==3:
+            # Genero las matrices, una columna y otra cuadrada de 3x3, vacias:
+            matrizB = np.zeros(3)
+            matrizA = np.zeros((3, 3))
+            y11=funcion31*funcion31
+            y12=funcion31*funcion32
+            y13=funcion31*funcion33
+            y22=funcion32*funcion32
+            y23=funcion32*funcion33
+            y33=funcion33*funcion33
+        
+            
+            # Calculo de Matriz A:
+            # para definir las funciones que van en la intregal
+            # las calcule cada una.
 
-        y12=funcion31*funcion32
-        a12=sp.integrate(y12,(x,c,d))
+            matrizA[0][0] = sp.integrate(y11,(x,c,d))
+            matrizA[0][1] = sp.integrate(y12,(x,c,d))
+            matrizA[0][2] = sp.integrate(y13,(x,c,d))
 
-        y13=funcion31*funcion33
-        a13=sp.integrate(y13,(x,c,d))
+            matrizA[1][0] = sp.integrate(y12,(x,c,d))
+            matrizA[1][1] = sp.integrate(y22,(x,c,d))
+            matrizA[1][2] = sp.integrate(y23,(x,c,d))
 
-        y22=funcion32*funcion32
-        a22=sp.integrate(y22,(x,c,d))
+            matrizA[2][0] = sp.integrate(y13,(x,c,d))
+            matrizA[2][1] =sp.integrate(y23,(x,c,d))
+            matrizA[2][2] = sp.integrate(y33,(x,c,d))
+                    
+            print('Matriz A = ',matrizA)
+            #matriz columna B
+            l1=funcion*funcion31
+            l2=funcion*funcion32
+            l3=funcion*funcion33
+            
+            matrizB[0] = sp.integrate(l1,(x,c,d))
+            matrizB[1] = sp.integrate(l2,(x,c,d))
+            matrizB[2] = sp.integrate(l3,(x,c,d))
 
-        y23=funcion32*funcion33
-        a23=sp.integrate(y23,(x,c,d))
+            print('Matriz B = ',matrizB)
+            
+            # Resuelvo el sistema de ecuaciones
+            matrizAInv = np.linalg.inv(matrizA)
+            matrizX = np.matmul(matrizAInv,matrizB)
 
-        y33=funcion33*funcion33
-        a33=sp.integrate(y33,(x,c,d))
+            print('Coeficientes = ',matrizX)
+            print ('La mejor aproximacion de la funicon es:')
 
-        m1 = np.array([[a11,a12,a13],[a12,a22,a23],[a13,a23,a33]])
-
-        #matriz columna de terminos independientes 
-
-        l1=funcion*funcion31
-        b1=sp.integrate(l1,(x,c,d))
+        else:
+            # Genero las matrices, una columna y otra cuadrada , vacias:
+            matrizB = np.zeros(2)
+            matrizA = np.zeros((2, 2))
 
 
-        l2=funcion*funcion32
-        b2=sp.integrate(l2,(x,c,d))
+            y11=funcion41*funcion41
+            y12=funcion41*funcion42
+            y22=funcion42*funcion42
 
-        l3=funcion*funcion33
-        b3=sp.integrate(l3,(x,c,d))
+            matrizA[0][0] = sp.integrate(y11,(x,c,d))
+            matrizA[0][1] = sp.integrate(y12,(x,c,d))
+            matrizA[1][0] = sp.integrate(y12,(x,c,d))
+            matrizA[1][1] = sp.integrate(y22,(x,c,d))
+            print('Matriz A = ',matrizA)
 
-        m2 = np.array([b1,b2,b3])
 
-        # resolver el sistema 
-        z=np.linalg.solve(m1,m2)
-        print(z)
-        print('La solución del sistema asociado al problemas es: {z}')
+            #matriz columna B 
+            l1=funcion*funcion41
+            l2=funcion*funcion42
+
+            matrizB[0] = sp.integrate(l1,(x,c,d))
+            matrizB[1] = sp.integrate(l2,(x,c,d))
+            print('Matriz B = ',matrizB)
+
+            # Resuelvo el sistema de ecuaciones
+            matrizAInv = np.linalg.inv(matrizA)
+            matrizX = np.matmul(matrizAInv,matrizB)
+
+            print('Coeficientes = ',matrizX)
+            print ('La mejor aproximacion de la funicon es:')
+
+
+            
+        
+
+
+
+
+        
 
 
 
